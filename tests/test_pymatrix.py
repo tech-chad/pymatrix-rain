1ea998265a127259ef8e6259d7f866e2be04bfaa
Chad Larson <techchad2@gmail.com>
2019-09-22 17:07:09 -0500

    Added test_mode to some of the tests to prevent some random fails

--- tests/test_pymatrix.py
+++ tests/test_pymatrix.py
@@ -10,36 +10,37 @@ def pymatrix_run(*args):
     return ["python3", "pymatrix.py"] + options
 
 
+def test_pymatrix_screen_test_mode():
+    with Runner(*pymatrix_run("--test_mode")) as h:
+        h.await_text("T")
+
+
 @pytest.mark.parametrize("test_key", ["Q", "q"])
 def test_pymatrix_quit(test_key):
-    with Runner(*pymatrix_run()) as h:
-        h.await_text("a")
+    with Runner(*pymatrix_run("--test_mode")) as h:
+        h.await_text("T")
         h.write(test_key)
         h.press("Enter")
         h.await_exit()
 
 
 def test_pymatrix_screen_resize_width_very_narrow():
-    with Runner(*pymatrix_run(), width=50, height=50) as h:
-        h.await_text("a")
+    with Runner(*pymatrix_run("--test_mode"), width=50, height=50) as h:
+        h.await_text("T")
         h.tmux.execute_command('split-window', '-ht0', '-l', 48)
-        h.await_text("a")
+        h.await_text("T")
 
 
 def test_pymatrix_screen_resize_height_very_short():
-    with Runner(*pymatrix_run(), width=50, height=50) as h:
-        h.await_text("a")
+    with Runner(*pymatrix_run("--test_mode"), width=50, height=50) as h:
+        h.await_text("T")
         h.tmux.execute_command('split-window', '-vt0', '-l', 46)
-        h.await_text("a")
+        h.await_text("T")
 
 
 def test_pymatrix_screen_resize_height_too_short():
-    with Runner(*pymatrix_run(), width=50, height=50) as h:
-        h.await_text("a")
+    with Runner(*pymatrix_run("--test_mode"), width=50, height=50) as h:
+        h.await_text("T")
         h.tmux.execute_command('split-window', '-vt0', '-l', 47)
         h.await_text("Error screen height is to short.")
 
-
-def test_pymatrix_screen_test_mode():
-    with Runner(*pymatrix_run("--test_mode")) as h:
-        h.await_text("T")
