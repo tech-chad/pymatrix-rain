64b7737f401969edee3d67859b3a6d100d3ead28
Chad Larson <techchad2@gmail.com>
2019-09-22 16:37:29 -0500

    Bumped version to 0.1

--- pymatrix.py
+++ pymatrix.py
@@ -5,7 +5,7 @@ import sys
 from random import choice, randint
 from time import sleep
 
-version = "0.0"
+version = "0.1"
 
 char_list = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n",
              "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B",
